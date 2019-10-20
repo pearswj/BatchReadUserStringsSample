b4d90b63fab24a0c1c889efe619b1a4e55ebf2dd
Will Pearson <will@mcneel.com>
2019-10-20 12:34:08 -0400

    Update sample with workaround to update attrs
    Moves all objects to another layer

--- BatchReadUserStringsSample/Program.cs
+++ BatchReadUserStringsSample/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace BatchReadUserStringsSample
 {
@@ -40,17 +41,56 @@ namespace BatchReadUserStringsSample
         using (var doc = Rhino.FileIO.File3dm.Read(file))
         {
 
+          foreach(var layer in doc.AllLayers)
+          {
+            Console.WriteLine("Layer \"{0}\", with index: {1}", layer.Name, layer.Index);
+          }
+
           Console.WriteLine("Nº of objects in file: {0}", doc.Objects.Count);
+
+          var old = new List<Tuple<Rhino.Geometry.GeometryBase, Rhino.DocObjects.ObjectAttributes>>();
 
           foreach (var obj in doc.Objects)
           {
-            Console.WriteLine("Found object with id: {0}", obj.Id);
+            Console.WriteLine("Found object with id: {0}, on layer: {1}", obj.Id, obj.Attributes.LayerIndex);
             var us = obj.Attributes.GetUserStrings();
             foreach (string key in us.AllKeys)
             {
               Console.WriteLine("{0}: {1}", key, us[key]);
             }
+
+            old.Add(Tuple.Create(obj.Geometry.Duplicate(), obj.Attributes.Duplicate()));
           }
+
+          var new_layer_index = 1;
+          Console.WriteLine("Updating layer indices to: {0}", new_layer_index);
+
+          doc.Objects.Clear();
+
+          foreach (var pair in old)
+          {
+            var attr = pair.Item2;
+            attr.LayerIndex = new_layer_index;
+
+            var geom = pair.Item1;
+
+            switch (geom)
+            {
+              case Rhino.Geometry.Mesh mesh:
+                doc.Objects.AddMesh(mesh, attr);
+                break;
+              case Rhino.Geometry.Brep brep:
+                doc.Objects.AddBrep(brep, attr);
+                break;
+              case Rhino.Geometry.Curve curve:
+                doc.Objects.AddCurve(curve, attr);
+                break;
+              default:
+                break;
+            }
+          }
+
+          doc.Write(file + ".3dm", 6);
         }
       }
 
